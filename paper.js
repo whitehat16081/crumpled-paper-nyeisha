3b77bcddc78941e1b94292805e63f14de0fae20c
whitehat16081 <whitehat.16081@gmail.com>
2020-12-03 20:29:21 +0530

    changed files

--- paper.js
+++ paper.js
@@ -4,9 +4,9 @@ class Paper  {
             isStatic: false,
             restitution: 0.3,
             density: 1.2,
-            friction: 0.5,
+            friction: 0,
         }
-        this.body = Bodies.circle(x,y,23,options);
+        this.body = Bodies.circle(x,y,22,options);
     
         this.image = loadImage("paper.png");
         World.add(world,this.body);
--- sketch.js
+++ sketch.js
@@ -6,16 +6,13 @@ const Body = Matter.Body;
 
 var ground1,dustbin1,dustbin2,dustbin3,paper1
 
-function preload()
-{
-    paperimage = loadImage("paper.png")
-}
+
 
 function setup() {
 	
 	
 	var canvas = createCanvas(1300, 700);
-	rectMode(CENTER);
+	
 	engine = Engine.create();
 	world = engine.world;
 	
@@ -25,7 +22,7 @@ function setup() {
 //	dustbin1 = new Dustbin(1100,495,150,15);
 //	dustbin2 = new Dustbin(1185,420,15,170);
 //	dustbin3 = new Dustbin(1015,420,15,170);
-	dustbin4=new Dustbin(1050,435,160,155)
+	dustbin4=new Dustbin(1150,575,170,170)
 	ground1 = new Ground(700,600,1400,30);
 	
 	paper1 = new Paper(200,300);
